d90be3050fbcc7d141d0f866061843273a45ba03
Janne Gröndahl <janne.grondahl@aalto.fi>
2021-04-16 15:56:24 +0300

    Clean up unused scripts

--- scripts/remove_skips.sh
+++
@@ -1,3 +0,0 @@
-# python scripts/plot_distances.py -i data/processed/arcore-new-better-ds1000.jsonl
-python scripts/find_position_jumps.py -i data/processed/arcore-new-better-ds1000.jsonl
-# jq -c 'select(.time>15.0)' data/processed/arcore-new-better-ds1000.jsonl
